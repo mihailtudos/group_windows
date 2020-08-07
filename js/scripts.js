dad4f559bebf589b4f2b377545df0caf82a19ecd
Mihail Tudos <mihailtudos@outlook.com>
2020-08-07 16:10:22 +0100

    removed about section

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-const burger    = document.querySelector('#burger');
-const menu      = document.querySelector('#menu')
+const burger        = document.querySelector('#burger');
+const menu          = document.querySelector('#menu')
+const home_li       = document.getElementById('home_li');
+const products_li   = document.getElementById('products_li');
+const contacts_li   = document.getElementById('contacts_li');
 
 burger.addEventListener('click', () => {
     if(menu.classList.contains('hidden')) {
@@ -8,3 +11,24 @@ burger.addEventListener('click', () => {
         menu.classList.add('hidden');
     }
 });
+
+
+function active_products(){
+    contacts_li.classList.remove('active_item');
+    home_li.classList.remove('active_item');
+    products_li.classList.add('active_item');
+    
+}
+
+function active_home(){
+    contacts_li.classList.remove('active_item');
+    products_li.classList.remove('active_item');
+    home_li.classList.add('active_item');
+    
+}
+
+function active_contacts(){
+    home_li.classList.remove('active_item');
+    products_li.classList.remove('active_item');
+    contacts_li.classList.add('active_item');
+}
